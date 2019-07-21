cdd04bc2be31ace2e4b91e06a8167745a3feca84
mohamed khamis <mklhs66662000@gmail.com>
2019-07-21 06:19:03 +0200

    divide our page into menu and menu item components, finished styling our menu-item component

---
+++ src/pages/home-page/home-page-component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Menu from '../../components/menu/menu-component';
+import './home-page.scss';
+
+const HomePage= ()=>(
+<div className="homepage">
+    <Menu />
+</div>
+)
+
+export default HomePage;
