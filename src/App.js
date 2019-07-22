b8e7ae2878cbc45edacdab5831421c4bdacb2542
mohamed khamis <mklhs66662000@gmail.com>
2019-07-22 23:41:22 +0200

    created a new Shop page , collection Preview Component and Collection Item Component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/home-page/home-page-component';
-
-const Hats = ()=>(
-  <h1>This is hats Page</h1>
-);
+import ShopPage from './pages/shop-page/shop-page-component';
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage}/>
-      <Route path="/hats" component={Hats}/>
+      <Route path="/shop" component={ShopPage}/>
     </div>
   );
 }
