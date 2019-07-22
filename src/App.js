b2701f35a99d0bbe2e83321490a216d4d5f68ccc
mohamed khamis <mklhs66662000@gmail.com>
2019-07-22 14:42:46 +0200

    update menu with navigation using router and HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/home-page/home-page-component';
+
+const Hats = ()=>(
+  <h1>This is hats Page</h1>
+);
 
 function App() {
   return (
     <div>
-     <HomePage />
+      <Route exact path="/" component={HomePage}/>
+      <Route path="/hats" component={Hats}/>
     </div>
   );
 }
