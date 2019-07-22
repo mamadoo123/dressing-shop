15e756d92c720105be2cee991927ec2c7e987526
mohamed khamis <mklhs66662000@gmail.com>
2019-07-23 01:05:18 +0200

    added the header component to all pages and routing links inside the header

---
+++ src/components/header/header-component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './header.scss';
+import {ReactComponent as Logo} from '../../assets/crown.svg';
+
+const Header = ()=>(
+    <div className="header">
+        <Link to="/" className="logo-container">
+            <Logo className="logo"/>
+        </Link>
+        <div className="options">
+            <Link to="/shop" className="option">SHOP</Link>
+            <Link to="/shop" className="option">Contact</Link>
+        </div>
+    </div>
+)
+
+export default Header;
